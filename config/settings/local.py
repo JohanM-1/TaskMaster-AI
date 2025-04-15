07fb752c71933adb46a980f549319a09d5fd628a
Johan Monroy <145858899+JohanM-1@users.noreply.github.com>
2025-04-15 03:04:46 +0000

    Refactor ALLOWED_HOSTS to improve readability and maintainability

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,12 @@ SECRET_KEY = env(
     default="NQT2w9oFMF7RRmGccHideVjCeK55TQvZeubdx46Rl8VujjMBeiCwRWeB7b2QsoHT",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", env("HOST_IP")]  # noqa: S104
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+    env("HOST_IP", default="taskmaster-ai.techcreator.org"),
+]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
